4fb0582b5ccc7217bb69ef86403af5fa0e6288a5
ketrab100 <bartis300@gmail.com>
2022-04-24 13:46:45 +0200

    Generate JWT

---
+++ psiim/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace psiim.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReservationController : Controller
+    {
+        [Authorize]
+        public JsonResult Index()
+        {
+            return new JsonResult("user"+UserId());
+        }
+        protected string UserId()
+        {
+            var principal = HttpContext.User;
+            if (principal?.Claims != null)
+            {
+                foreach (var claim in principal.Claims)
+                {
+                    Log.Debug($"CLAIM TYPE: {claim.Type}; CLAIM VALUE: {claim.Value}");
+                }
+
+            }
+            return principal?.Claims?.SingleOrDefault(p => p.Type == "UserName")?.Value;
+        }
+    }
+}
---
+++ psiim/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace psiim.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TokenController : Controller
+    {
+        public IConfiguration _configuration;
+
+        public TokenController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(string userName, string password)
+        {
+            //create claims details based on the user information
+            var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserName", userName),
+                    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddMinutes(20),
+                signingCredentials: signIn);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
